6a7d5ab4a51c0930cd37d91b53df9c4425cde2a2
Curtis Menmuir <curtismenmuir@gmail.com>
2018-10-21 00:34:27 +0100

    TRI_API_3 - created C# Web API with UT project
    Inital commit of C# Web API:
    - Added Triangulator controller (HTTP GET will return "Hello World")
    - Added UT project

---
+++ Triangulator/Triangulator/Controllers/TriangulatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Triangulator.Controllers
+{
+    [Route("api/[controller]")]
+    public class TriangulatorController : ControllerBase
+    {
+        // GET: api/triangulator
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
